4eb8f9155e04c7dca6fb3c21d0005567b81729fd
JATIN KUMAR VERMA <82764118+jatinkumarverma@users.noreply.github.com>
2022-12-21 19:07:55 +0530

    first commit

---
+++ src/utils/dummy/latestTransactions.js
@@ -0,0 +1,33 @@
+export const latestTransactionsData = [
+    {
+      id: 1,
+      transaction: "Hostel Fee 2022",
+      paid_on: "5 July 2022",
+      amount: "₹200000",
+    },
+    {
+      id: 2,
+      transaction: "Tuition Fee 2022",
+      paid_on: "20 July 2022",
+      amount: "₹200000",
+    },
+    {
+      id: 3,
+      transaction: "Exam Fee 2022",
+      paid_on: "1 April 2022",
+      amount: "₹5000",
+    },
+    {
+      id: 4,
+      transaction: "Sports Fee",
+      paid_on: "3 March 2022",
+      amount: "₹20000",
+    },
+    {
+      id: 5,
+      transaction: "Library Fee",
+      paid_on: "10 March 2022",
+      amount: "₹20000",
+    },
+  ];
+  
---
+++ src/utils/dummy/pendingTransactions.js
@@ -0,0 +1,37 @@
+export const pendingTransactionsData = [
+  {
+    id: 1,
+    transaction: "Hostel Fee 2023",
+    due_date: "10 July 2023",
+    amount: "₹200000",
+    status: "pending",
+  },
+  {
+    id: 2,
+    transaction: "Tuition Fee 2023",
+    due_date: "10 July 2023",
+    amount: "₹200000",
+    status: "paid",
+  },
+  {
+    id: 3,
+    transaction: "Exam Fee 2022",
+    due_date: "1 December 2022",
+    amount: "₹5000",
+    status: "overdue",
+  },
+  {
+    id: 4,
+    transaction: "Sports Fee",
+    due_date: "3 March 2023",
+    amount: "₹20000",
+    status: "pending",
+  },
+  {
+    id: 5,
+    transaction: "Library Fee",
+    due_date: "10 March 2023",
+    amount: "₹20000",
+    status: "pending",
+  },
+];
